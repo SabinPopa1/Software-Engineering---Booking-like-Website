ed1d39054d8feec45f01b8968fab6e1393954d72
SabinPopa1 <125959059+SabinPopa1@users.noreply.github.com>
2025-03-27 14:11:01 +0200

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; 
+import {useParams} from 'react-router-dom';
+import Login from './userInteractions/Login';
+import Signup from './userInteractions/Signup';
+import Welcome from './userInteractions/Wellcome';
+import PanoramaTour from './model3D/PanoramaTour';
+import Hotels from './hotels/Hotels';
+import NotFoundPage from './NotFoundPage';
+import AttractionsPage from './attractions/Attractions';
+import AttractionDetailsPage from "./attractions/AttractionDetailsPage";
+import "./App.css"; 
+import UserProf from './userInteractions/UserProf';
+import HealthForm from './userInteractions/HealthForm';
+
+function HotelsPage() {
+  // Get the hotelId param from the URL.
+  let { hotelId } = useParams();
+  // ...
+}
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/userProf" element={<UserProf />} />
+        <Route path="/health-form" element={<HealthForm />} />
+        {/* <Route path="/register" element={<RegisterPage />} /> */}
+        <Route path="/panoramas" element={<PanoramaTour />} />
+        <Route path="/hotels">
+          <Route path=":hotelId" element={<Hotels />} />
+        </Route>
+        <Route path='/map' element={<Map />} />
+        <Route path='/check-auth' />
+        <Route path='/logout' />
+        <Route path='/saveHealthForm' />
+        <Route path='/get-logged-user-info' />
+        <Route path='/update-logged-user-info' />
+        <Route path="/attractions" element={<AttractionsPage />} />
+        <Route
+          path="/attractions/:hotelName"
+          element={<AttractionDetailsPage />}
+        />{" "}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App
